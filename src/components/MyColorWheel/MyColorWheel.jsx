3b381a9613d9c62ae193a7d25c9593534a6fc324
aGregAdams <gregory.adams29@gmail.com>
2025-07-07 19:14:53 -0400

    Increased size of the selected color pointer on color wheel

--- src/components/MyColorWheel/MyColorWheel.jsx
+++ src/components/MyColorWheel/MyColorWheel.jsx
@@ -11,6 +11,21 @@ const MyColorWheel = () => {
                 color={color.hex}
                 onChange={setColor}
                 style={{ width: 200, height: 200 }}
+                pointer={({ color, style }) => (
+                    <div style={style}>
+                        <div
+                            style={{
+                                width: 24,    // Set your desired width
+                                height: 24,   // Set your desired height
+                                transform: 'translate(-12px, -12px)', // Center the circle
+                                borderRadius: '50%',
+                                border: '2px solid #fff',
+                                background: color,
+                                boxShadow: '0 0 4px rgba(0,0,0,0.3)',
+                            }}
+                        />
+                    </div>
+                )}
             />
             <p className="color-wheel-selected">{color.hex.toUpperCase()}</p>
             <div className="color-wheel-color"

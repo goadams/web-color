3e65a35010705ccb76678b289de2bc647a6af3b3
aGregAdams <gregory.adams29@gmail.com>
2025-07-15 14:19:49 -0400

    Created reusable InputSlider.jsx

---
+++ src/components/InputSlider/InputSlider.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./InputSlider.css"
+
+const InputSlider = (props) => {
+    const {
+        val,
+        handleChange,
+        min = 0,
+        max = 100,
+        label,
+        id,
+        handleIncrement,
+        handleDecrement
+    } = props;
+
+    return (
+        <div className="input-slider">
+            {label && (
+                <label {...( id ? { htmlFor: id }: {})}>
+                    {label}
+                </label>
+            )}
+            {handleIncrement && handleDecrement && (
+                <div className="slider-btns">
+                    <button onClick={handleDecrement}>-</button>
+                    <button onClick={handleIncrement}>+</button>
+                </div>
+            )}
+            <input
+                className="slider"
+                type="range"
+                min={min}
+                max={max}
+                value={val}
+                onChange={handleChange}
+                {...( id ? { id } : {})}
+            />
+        </div>
+)};
+
+
+export default InputSlider;
